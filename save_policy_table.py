d6185d5e2a503baa2328675b84425088140a3d52
bamboo_chen <b10902005@ntu.edu.tw>
2025-03-14 02:33:42 +0800

    update policy

--- save_policy_table.py
+++ save_policy_table.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 import itertools
+import random
 
 
 def save_policy_table(policy_table):
@@ -8,10 +9,10 @@ def save_policy_table(policy_table):
     for state in policy_table.keys():
         p = policy_table[state]
         r = range(2)
-        m = np.min(p)
+        m = random.uniform(-10, -9)
         for i, j, k, l in itertools.product(range(2), repeat=4):
             ns = (state[0], state[1], i, j, k, l)
-            pp = [m if i else p[0], m if j else p[1], m if k else p[2], m if l else p[3]]
+            pp = [m if j else p[0], m if i else p[1], m if k else p[2], m if l else p[3]]
             new_table[str(ns)] = pp
     with open('policy.json', 'w') as f:
         json.dump(new_table, f)
