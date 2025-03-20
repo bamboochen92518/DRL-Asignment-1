e80e0764bb81f9d6a8a098f33214f74f464be688
bamboo_chen <b10902005@ntu.edu.tw>
2025-03-20 16:06:05 +0800

    add random seed

--- student_agent.py
+++ student_agent.py
@@ -1,8 +1,6 @@
 # Remember to adjust your student ID in meta.xml
 import numpy as np
 import json
-import random
-import gym
 import itertools
 
 
@@ -10,6 +8,7 @@ is_picked = False
 visited = {"R": 0, "G": 0, "Y": 0, "B": 0}
 destination = None
 DEBUG = False
+np.random.seed(42)
 
 
 def softmax(x):
@@ -18,13 +17,13 @@ def softmax(x):
 
 
 def my_state(obs):
-    
+
     # TODO: Train your own agent
     # HINT: If you're using a Q-table, consider designing a custom key based on `obs` to store useful information.
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
-    #       To prevent crashes, implement a fallback strategy for missing keys. 
+    #       To prevent crashes, implement a fallback strategy for missing keys.
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
-    
+
     # Load global variable
     global is_picked
     global visited
@@ -32,7 +31,8 @@ def my_state(obs):
 
     taxi_row, taxi_col, Rx, Ry, Gx, Gy, Yx, Yy, Bx, By, obstacle_north, obstacle_south, obstacle_east, obstacle_west, passenger_look, destination_look = obs
 
-    destination_to_loc = {'R': (Rx, Ry), 'G': (Gx, Gy), 'Y': (Yx, Yy), 'B': (Bx, By)}
+    destination_to_loc = {'R': (Rx, Ry), 'G': (
+        Gx, Gy), 'Y': (Yx, Yy), 'B': (Bx, By)}
 
     # Determine where to go
     def determine_target():
@@ -58,7 +58,7 @@ def my_state(obs):
             else:
                 target = (Bx, By)
         return target
-    
+
     # Determine the spot
     def on_location():
         taxi_loc = (taxi_row, taxi_col)
@@ -71,7 +71,7 @@ def my_state(obs):
         if taxi_loc == (Bx, By):
             return 'B'
         return None
-    
+
     curr_target = determine_target()
     reward = 0
     direction = (taxi_row - curr_target[0], taxi_col - curr_target[1])
@@ -82,12 +82,12 @@ def my_state(obs):
     # Not Arrive Yet
     if direction != (0, 0):
         return direction, reward, obstacle_north, obstacle_south, obstacle_east, obstacle_west
-    
+
     # Arrive
     reward = 1
 
     if DEBUG:
-        print(f"GOAL")
+        print("GOAL")
 
     # Passenger is not picked yet
     if not is_picked:
@@ -100,7 +100,7 @@ def my_state(obs):
             visited['B'] = 1
             visited[on_location()] += 1
             if DEBUG:
-                print(f"PICK")
+                print("PICK")
             return direction, reward, obstacle_north, obstacle_south, obstacle_east, obstacle_west
         else:
             if destination_look:
@@ -116,7 +116,7 @@ def my_state(obs):
     if destination_look:
         direction = 5
         if DEBUG:
-            print(f"DONE")
+            print("DONE")
         return direction, reward, obstacle_north, obstacle_south, obstacle_east, obstacle_west
 
     visited[on_location()] += 1
@@ -128,26 +128,29 @@ def my_state(obs):
 
 
 def get_action(obs):
-    direction, _, obstacle_north, obstacle_south, obstacle_east, obstacle_west = my_state(obs)
+    direction, _, obstacle_north, obstacle_south, obstacle_east, obstacle_west = my_state(
+        obs)
 
     if isinstance(direction, int):
         return direction
 
     while abs(direction[0]) > 4 or abs(direction[1]) > 4:
         direction = (direction[0] // 2, direction[1] // 2)
-    
-    state = str((direction, obstacle_north, obstacle_south, obstacle_east, obstacle_west))
+
+    state = str((direction, obstacle_north, obstacle_south,
+                obstacle_east, obstacle_west))
 
     with open('policy.json', 'r') as f:
         policy_table = json.load(f)
-    
+
     if state not in policy_table.keys():
         prob = np.zeros(4)
         cnt = 0
         for x, y in itertools.product(range(-4, 4), repeat=2):
             if x * direction[0] < 0 or y * direction[1] < 0:
                 continue
-            ns = ((x, y), obstacle_north, obstacle_south, obstacle_east, obstacle_west)
+            ns = ((x, y), obstacle_north, obstacle_south,
+                  obstacle_east, obstacle_west)
             if str(ns) in policy_table.keys():
                 prob += softmax(policy_table[str(ns)])
                 cnt += 1
@@ -155,5 +158,5 @@ def get_action(obs):
 
     else:
         action_prob = softmax(policy_table[state])
-    
-    return np.random.choice(len(action_prob), p=action_prob) # Choose a random action
+
+    return np.random.choice(len(action_prob), p=action_prob)
